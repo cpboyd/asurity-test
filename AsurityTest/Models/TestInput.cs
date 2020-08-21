e3056f55afe33a4dd6d93491eead38118470c656
Christopher Boyd <xpboyd@gmail.com>
2020-08-21 08:46:42 -0400

    more comments

--- AsurityTest/Models/TestInput.cs
+++ AsurityTest/Models/TestInput.cs
@@ -6,7 +6,9 @@ namespace AsurityTest.Models
     public class TestInput
     {
         public StateEnum State = StateEnum.Unspecified;
+        // Total loan amount:
         public double Total;
+        // APR as percentage:
         public double Apr;
         public IEnumerable<Fee> Fees;
         public LoanType Type = LoanType.Unspecified;
